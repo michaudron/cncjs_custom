2bf2bfff9d6dd4cc656465fe5a238720de0decae
Ron Michaud <michaudron@yahoo.com>
2021-03-14 10:36:33 -0400

    drop all non grbl related controllers

--- src/app/containers/Workspace/Widget.jsx
+++ src/app/containers/Workspace/Widget.jsx
@@ -7,12 +7,9 @@ import GCodeWidget from 'app/widgets/GCode';
 import GrblWidget from 'app/widgets/Grbl';
 import LaserWidget from 'app/widgets/Laser';
 import MacroWidget from 'app/widgets/Macro';
-import MarlinWidget from 'app/widgets/Marlin';
 import ProbeWidget from 'app/widgets/Probe';
-import SmoothieWidget from 'app/widgets/Smoothie';
 import SpindleWidget from 'app/widgets/Spindle';
 import CustomWidget from 'app/widgets/Custom';
-import TinyGWidget from 'app/widgets/TinyG';
 import VisualizerWidget from 'app/widgets/Visualizer';
 import WebcamWidget from 'app/widgets/Webcam';
 import SwitchesWidget from 'app/widgets/Switches';
@@ -26,12 +23,9 @@ const getWidgetByName = (name) => {
         'grbl': GrblWidget,
         'laser': LaserWidget,
         'macro': MacroWidget,
-        'marlin': MarlinWidget,
         'probe': ProbeWidget,
-        'smoothie': SmoothieWidget,
         'spindle': SpindleWidget,
         'custom': CustomWidget,
-        'tinyg': TinyGWidget,
         'visualizer': VisualizerWidget,
         'webcam': WebcamWidget,
         'switches': SwitchesWidget
